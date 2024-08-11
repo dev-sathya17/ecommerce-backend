68fd75294c131724b0955753cd4b0b50422f773c
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-08-11 19:03:15 +0530

    connection to mongodb established

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+const { MONGODB_URI, PORT } = require("./utils/config");
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
